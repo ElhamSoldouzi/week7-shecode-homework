c59a2b2f6c210e9aae284808f40419e4c12a1754
ElhamSoldouzi <134371922+ElhamSoldouzi@users.noreply.github.com>
2024-11-29 13:21:13 +0330

    add apiUrl

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
+function searchCity(city) {
+  let apiKey = "4e4452b3b8e30dte63o4ebba04a0fef4";
+  apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  console.log(apiUrl);
+}
+
 function searchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
