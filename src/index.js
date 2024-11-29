70f889190f85e11366f97e562833341374523a68
ElhamSoldouzi <134371922+ElhamSoldouzi@users.noreply.github.com>
2024-11-29 13:24:31 +0330

    fixing a mistyped

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 function searchCity(city) {
   let apiKey = "4e4452b3b8e30dte63o4ebba04a0fef4";
-  apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   console.log(apiUrl);
 }
 
