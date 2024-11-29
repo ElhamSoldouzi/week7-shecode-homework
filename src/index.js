25390d8ad705536fde6a2c866024399ccbb744f7
ElhamSoldouzi <134371922+ElhamSoldouzi@users.noreply.github.com>
2024-11-29 12:25:18 +0330

    updating javascript

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 function searchSubmit(event) {
   event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = searchInput.value;
 }
 
 let searchFormElement = document.querySelector("#search-form");
