7fc84299f7752af672e3dc05106b4e772a742362
ElhamSoldouzi <134371922+ElhamSoldouzi@users.noreply.github.com>
2024-12-02 20:21:31 +0330

    update js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
+function updateWeatherInfo(response) {
+  let temperatureNumberElement = document.querySelector("#number");
+  temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureNumberElement.innerHTML = Math.round(temperature);
+}
+
 function searchCity(city) {
   let apiKey = "4e4452b3b8e30dte63o4ebba04a0fef4";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-  console.log(apiUrl);
+  axios.get(apiUrl).then(updateWeatherInfo);
 }
 
 function searchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+
   searchCity(searchInput.value);
 }
 
