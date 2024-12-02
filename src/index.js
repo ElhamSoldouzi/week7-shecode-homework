cb75e371c7f32a80205c6b97df7f84c876991e09
ElhamSoldouzi <134371922+ElhamSoldouzi@users.noreply.github.com>
2024-12-02 22:13:25 +0330

    adding icon elements

--- src/index.js
+++ src/index.js
@@ -7,6 +7,13 @@ function updateWeatherInfo(response) {
   let speedElement = document.querySelector("#wind-speed");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
+
+  iconElement.innerHTML = ` <img
+              src="${response.data.condition.icon_url}"
+              class="weather-icon"
+            />
+`;
 
   timeElement.innerHTML = formatDate(date);
   cityElement.innerHTML = response.data.city;
